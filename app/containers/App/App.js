82f6e4c6b71dbe6b0252287c8d8f8ae36c7d331d
AudrenGuillaume <audren.guilllaume@gmail.com>
2018-06-02 17:22:00 +0200

    same

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -10,21 +10,12 @@ import React, { Component, Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
 
-<<<<<<< HEAD
-import MainPage from '../Pages/MainPage/Loadable';
-import HomePage from '../Pages/HomePage/Loadable';
-import LoginPage from '../Pages/LoginPage/Loadable';
-import FeaturePage from '../Pages/FeaturePage/Loadable';
-import ContactPage from '../Pages/ContactPage/Loadable';
-import NotFoundPage from '../Pages/NotFoundPage/Loadable';
-=======
 import MainPage from 'containers/MainPage/Loadable';
 import HomePage from 'containers/HomePage/Loadable';
 import LoginPage from 'containers/LoginPage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import ContactPage from 'containers/ContactPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
->>>>>>> 3968a8f0a6a581205832ca8e2f9c0fe91aa70210
 
 import Navibar from '../../components/Navigation';
 import Footer from 'components/Footer';
@@ -51,11 +42,7 @@ class App extends Component {
           <meta name="description" content="Actuality page" />
         </Helmet>
         <Layout>
-<<<<<<< HEAD
         <Header id="header"> <Navibar /></Header>  
-=======
-        {current_path !== '/' ? (<Header id="header"><Navibar /></Header>) : null}
->>>>>>> 3968a8f0a6a581205832ca8e2f9c0fe91aa70210
         <Switch>
           <Route exact path="/" component={HomePage} />  
           <Route path="/login" component={LoginPage} />
@@ -75,11 +62,6 @@ class App extends Component {
 
 export default App;
 
-
-<<<<<<< HEAD
 // <Route exact path="/home" component={HomePage} />
 
 // {current_path !== '/' ? (<Header id="header"><Navibar /></Header>) : null}
-=======
-// <Route exact path="/home" component={HomePage} />
->>>>>>> 3968a8f0a6a581205832ca8e2f9c0fe91aa70210
